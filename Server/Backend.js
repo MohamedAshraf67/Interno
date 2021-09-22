a1e4bec199d891fc95c498dd386e095a2fd5a606
MahmoudMorad7 <87903231+MahmoudMorad7@users.noreply.github.com>
2021-09-22 18:27:28 +0200

    ez

--- Server/Backend.js
+++ Server/Backend.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
 const port = 3000
-const cors = require("cors")
+const cors = require("cors");
+const login = require("../models/login");
+const Signup = require("../models/signup");
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -13,4 +15,27 @@ app.get("/", cors(), async (req, res) => {
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
+})
+
+app.post('/login',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+
+    user.findOne({username:username,password:password},function(err,user){
+        if (err){
+            console.log(err);
+        }
+    })
+})
+
+app.post('/signup',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    var email=req.body.email;
+
+    var user=new Signup();
+    user.username=username;
+    user.password=password;
+    user.email=email;
+
 })
